4a7fac9d44c19f98454aeb3551ca5069ba160dd0
Peter <petervsjim@gmail.com>
2022-02-08 15:32:09 +0800

    finish desktop implementation

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,27 @@ import "./css/App.css";
 import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
 import { FiMinusCircle, FiPlusCircle } from "react-icons/fi";
 import metronome from "./sound/metronome.wav";
+import Slider from "@mui/material/Slider";
+import { createTheme } from "@mui/material/";
+import { cyan, green } from "@mui/material/colors";
+import { ThemeProvider } from "@emotion/react";
+
+const metronomeSound = new Audio(metronome);
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: cyan[700],
+      },
+      secondary: {
+        main: green[500],
+      },
+    },
+  });
+
   const [pitch, setPitch] = useState<number>(80);
-
   let bpm = (60 * 1000) / pitch;
-
-  const metronomeSound = new Audio(metronome);
 
   const [intervalID, setIntervalID] = useState<NodeJS.Timer>(
     setInterval(() => {}, 10)
@@ -17,13 +31,13 @@ function App() {
 
   const [nowPlaying, setNowPlaying] = useState<boolean>(false);
 
-  const handlePlay = () => {
+  const handlePlay = (isPlaying: boolean) => {
+    if (nowPlaying) return;
     setNowPlaying(true);
     metronomeSound.play();
     setIntervalID(
       setInterval(() => {
         metronomeSound.play();
-        console.log(intervalID);
       }, bpm)
     );
   };
@@ -34,52 +48,73 @@ function App() {
   };
 
   const handleAddPitch = () => {
-    if (nowPlaying) {
-      clearInterval(intervalID);
-      setPitch((prevPitch) => prevPitch + 1);
-      handlePlay();
-    } else {
-      setPitch((prevPitch) => prevPitch + 1);
-    }
+    setPitch((prevPitch) => prevPitch + 1);
   };
 
   const handleMinusPitch = () => {
-    if (nowPlaying) {
-      clearInterval(intervalID);
-      setPitch((prevPitch) => prevPitch - 1);
-      handlePlay();
-    } else {
-      setPitch((prevPitch) => prevPitch - 1);
-    }
+    setPitch((prevPitch) => prevPitch - 1);
   };
+
+  const handleChange = (e: any) => {
+    setPitch(e.target.value);
+  };
+
+  function buttonActive() {
+    if (nowPlaying) {
+      return (
+        <AiFillPauseCircle
+          size={120}
+          className="button control__button"
+          onClick={() => handlePause()}
+        />
+      );
+    } else {
+      return (
+        <AiFillPlayCircle
+          size={120}
+          className="button control__button"
+          onClick={() => handlePlay(nowPlaying)}
+        />
+      );
+    }
+  }
 
   return (
     <>
-      <h1>{pitch}</h1>
-      <div className="control">
-        <AiFillPlayCircle
-          size={80}
-          className="button control__play"
-          onClick={() => handlePlay()}
-        />
-        <AiFillPauseCircle
-          size={80}
-          className="button control__pause"
-          onClick={() => handlePause()}
-        />
-      </div>
-      <div className="speed">
-        <FiMinusCircle
-          size={80}
-          className="button speed__minus"
-          onClick={() => handleMinusPitch()}
-        />
-        <FiPlusCircle
-          size={80}
-          className="button speed__plus"
-          onClick={() => handleAddPitch()}
-        />
-      </div>
+      <ThemeProvider theme={theme}>
+        <h1 className="pitch">
+          {pitch}
+          <span className="pitch__bpm">BPM</span>
+        </h1>
+        <div className="speed">
+          <FiMinusCircle
+            size={80}
+            className="button speed__minus"
+            onClick={() => handleMinusPitch()}
+          />
+          <Slider
+            className="speed__slider"
+            value={pitch}
+            onChange={handleChange}
+            aria-label="Default"
+            valueLabelDisplay="auto"
+            min={50}
+            max={250}
+            sx={[
+              { height: 20 },
+              { ".MuiSlider-thumb": { height: 35, width: 35 } },
+            ]}
+          />
+          <FiPlusCircle
+            size={80}
+            className="button speed__plus"
+            onClick={() => handleAddPitch()}
+          />
+        </div>
+        <div className="control">
+          {buttonActive()}
+        </div>
+      </ThemeProvider>
     </>
   );
 }
--- src/decs.d.ts
+++ src/decs.d.ts
@@ -1,1 +1,14 @@
 declare module "*.wav"
+declare module '@mui/material/styles' {
+  interface Theme {
+    status: {
+      danger: string;
+    };
+  }
+  // 允许配置文件使用 `createTheme`
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    };
+  }
+}
